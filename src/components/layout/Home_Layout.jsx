ddf82e15ef4417d690f38b6ad0e04b8f3015265f
tkabir3066 <tutulkabir@gmail.com>
2025-09-27 19:13:12 +0530

    Initial commit

---
+++ src/components/layout/Home_Layout.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
+import Hero from "../Hero";
+import Category from "../Category";
+import { Outlet } from "react-router-dom";
+
+const Home_Layout = () => {
+  return (
+    <div>
+      <header>
+        <Navbar></Navbar>
+        <Hero></Hero>
+      </header>
+      <section>
+        <Category></Category>
+        <Outlet></Outlet>
+      </section>
+      <footer>
+        <Footer></Footer>
+      </footer>
+    </div>
+  );
+};
+
+export default Home_Layout;
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home_Layout from "./components/layout/Home_Layout.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home_Layout></Home_Layout>,
+    children: [],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+);
